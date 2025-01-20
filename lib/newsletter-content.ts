0ea2ee9c958546c8c5a5c8beb16cee8f7da2786f
Nicholas <rocketreneur@gmail.com>
2025-01-20 15:43:40 +0200

    commented out newsletter signup

--- lib/newsletter-content.ts
+++ lib/newsletter-content.ts
@@ -1,16 +1,16 @@
 // import { sql } from '@vercel/postgres';
-import { NewsletterContent } from '@/types/newslletter';
+// import { NewsletterContent } from '@/types/newslletter';
 
-export async function saveNewsletter(content: NewsletterContent) {
-  return await sql`
-    INSERT INTO newsletter_content (
-      subject, preview_text, content, publish_date, status
-    ) VALUES (
-      ${content.subject},
-      ${content.previewText},
-      ${JSON.stringify(content.content)},
-      ${content.publishDate},
-      ${content.status}
-    )
-  `;
-}
+// export async function saveNewsletter(content: NewsletterContent) {
+//   return await sql`
+//     INSERT INTO newsletter_content (
+//       subject, preview_text, content, publish_date, status
+//     ) VALUES (
+//       ${content.subject},
+//       ${content.previewText},
+//       ${JSON.stringify(content.content)},
+//       ${content.publishDate},
+//       ${content.status}
+//     )
+//   `;
+// }
