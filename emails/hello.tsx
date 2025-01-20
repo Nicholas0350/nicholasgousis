96ac755a84df81f60ac870b377bd720f95c2df1e
Nicholas <rocketreneur@gmail.com>
2025-01-20 15:41:37 +0200

    commented out newsletter signup

--- emails/hello.tsx
+++ emails/hello.tsx
@@ -1,5 +1,5 @@
 import { Html, Button, Container, Section, Text, Img } from "@react-email/components";
-import Unsubscribe from "./unsubscribe";
+// import Unsubscribe from "./unsubscribe";
 export default function Hello() {
   return (
     <Html>
@@ -37,7 +37,7 @@ export default function Hello() {
             Subscribe Now
           </Button>
 
-          <Unsubscribe />
+          {/* <Unsubscribe /> */}
         </Section>
       </Container>
     </Html>
